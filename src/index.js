0aee096added057067b1e8573e9b4c38196285b9
Renata <murzinarenata@gmail.com>
2025-06-02 16:12:45 -0700

    wave 4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const cityNameResetBtn = document.getElementById("cityNameReset");
 const headerCityName = document.getElementById("headerCityName");
 const tempValue = document.getElementById("tempValue");
 const gardenEmoji = document.getElementById("gardenContent");
+const currentTempButton = document.getElementById("currentTempButton");
+
 let currentTemp = 70;
 
 function updateCityName() {
@@ -31,7 +33,7 @@ const updateTempEmoji = () => {
     } else if (currentTemp >= 70) {
         tempValue.classList.add("orange");
         gardenEmoji.textContent = "🌸🦋🌼🌺🐝";
-     } else if (currentTemp >= 60) {
+    } else if (currentTemp >= 60) {
         tempValue.classList.add("yellow");
         gardenEmoji.textContent = "🌲🦌🍄🌳🪵";
     } else {
@@ -49,6 +51,47 @@ document.getElementById('decreaseTempControl').addEventListener('click', () => {
     currentTemp--;
     updateTempEmoji();
 })
+
+currentTempButton.addEventListener('click', () => {
+    const city = headerCityName.textContent
+
+    if (city) {
+        getLatandLon(city);
+    } else {
+        alert("Please enter a city name!");
+    }
+})
+
+const getLatandLon = (city) => {
+    axios .get('http://localhost:5000/location', {
+        params : { q: city }
+    })
+    .then((response) => {
+        const lat = response.data[0]['lat'];
+        const lon = response.data[0]['lon'];
+        getRealWeather(lat, lon);
+    })
+    .catch((error) => {
+        console.log('Error:', error)
+        alert('Enter right city name!')
+    })
+}
+
+const getRealWeather = (lat, lon) => {
+    axios
+        .get('http://localhost:5000/weather', {
+            params: { lat, lon }
+        })
+        .then((response) =>{
+            const tempKelvin = response.data.main.temp;
+            currentTemp = Math.round((tempKelvin - 273.15) * 9 / 5 + 32);
+            updateTempEmoji()
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('Could not get weather data.');
+        });
+}
 
 updateTempEmoji()
 
